d9d5c3c0d335915887870c0c77da294e6628c4da
Ammad Shakoor <ammadshakoor44@gmail.com>
2018-03-31 10:48:33 +0500

    Variables
    Variables are nothing but reserved memory locations to store values. This means that when you create a variable you reserve some space in memory.
    
    Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to variables, you can store integers, decimals or characters in these variables.

---
+++ variable.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+
+counter = 100          # An integer assignment
+
+miles   = 1000.0       # A floating point
+
+name    = "Ammad"     # A string
+
+
+
+print counter
+
+print miles
+
+print name
